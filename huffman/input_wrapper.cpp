fc702e2490d9546cb4dcabc8a3a14035f659d2dd
Danila Belous <belous.dp@gmail.com>
2022-08-22 10:59:00 +0300

    input wrapper refactoring

--- huffman/input_wrapper.cpp
+++ huffman/input_wrapper.cpp
@@ -6,49 +6,52 @@
 #include <cassert>
 #include <iostream>
 
-input_wrapper::input_wrapper(std::istream& input, size_t chunk_size,
-                             uint8_t nlb)
-    : input(input), chunk_size(chunk_size), pos(0), nlb(nlb) {
-  if (nlb > bit_sequence::WORD_WIDTH) {
-    throw std::invalid_argument("number of useful bits in the last word "
-                                "cannot be greater that word width");
-  }
+using bs = bit_sequence;
+
+input_wrapper::input_wrapper(std::istream& input) : input(input) {}
+
+input_wrapper::input_wrapper(std::istream& input, uint8_t unused)
+    : input(input), unused(unused) {
+  assert(unused < bs::WORD_WIDTH);
 }
 
 bool input_wrapper::has(size_t n) {
-  if (buf.size() - pos < n) {
-    fetch(std::max(chunk_size, n));
+  if (buf_size() < n) {
+    fetch();
   }
-  return buf.size() - pos >= n;
+  return buf_size() >= n;
 }
 
-void input_wrapper::fetch(size_t n) {
+size_t input_wrapper::buf_size() const {
+  return buf.size() - pos;
+}
+
+void input_wrapper::fetch() {
   if (!input) {
     return;
   }
   bit_sequence nbuf;
-  for (size_t i = pos; i + bit_sequence::WORD_WIDTH < buf.size(); ++i) {
+  for (size_t i = pos; i + bs::WORD_WIDTH < buf.size(); ++i) {
     nbuf.append_word(buf.word_at(i));
   }
-  for (size_t i = buf.size() < bit_sequence::WORD_WIDTH
+  for (size_t i = buf.size() < bs::WORD_WIDTH
                     ? pos
-                    : std::max(buf.size() - bit_sequence::WORD_WIDTH, pos);
+                    : std::max(buf.size() - bs::WORD_WIDTH, pos);
        i < buf.size(); ++i) {
     nbuf.append_bit(buf.bit_at(i));
   }
   buf = nbuf;
   pos = 0;
-  while (buf.size() < n) {
-    bit_sequence::word w = input.get();
+  while (buf_size() < CHUNK_SIZE) {
+    word w = input.get();
     if (!input) {
       return;
     }
     buf.append_word(w);
     if (eof()) {
-      for (size_t i = 0; i < bit_sequence::WORD_WIDTH - nlb; ++i) {
+      for (size_t i = 0; i < unused; ++i) {
         buf.pop_back_bit();
       }
-      nlb = 0;
     }
   }
 }
@@ -58,17 +61,17 @@ bool input_wrapper::eof() {
 }
 
 uint8_t input_wrapper::scan_bit() {
-  if (!has(1)) {
+  if (!has()) {
     throw std::invalid_argument("bad input request");
   }
   return buf.bit_at(pos++);
 }
 
 bit_sequence::word input_wrapper::scan_word() {
-  if (!has(bit_sequence::WORD_WIDTH)) {
+  if (!has(bs::WORD_WIDTH)) {
     throw std::invalid_argument("bad input request");
   }
-  bit_sequence::word res = buf.word_at(pos);
-  pos += bit_sequence::WORD_WIDTH;
+  word res = buf.word_at(pos);
+  pos += bs::WORD_WIDTH;
   return res;
 }
--- huffman/input_wrapper.h
+++ huffman/input_wrapper.h
@@ -7,21 +7,27 @@
 
 #include "bit_sequence.h"
 #include <istream>
-struct input_wrapper {
-  explicit input_wrapper(std::istream& input, size_t chunk_size, uint8_t nlb);
 
-  bool has(size_t n);
+struct input_wrapper {
+  using word = bit_sequence::word;
+
+  explicit input_wrapper(std::istream& input);
+  input_wrapper(std::istream& input, uint8_t unused);
+
+  bool has(size_t n = 1);
   uint8_t scan_bit();
-  bit_sequence::word scan_word();
+  word scan_word();
 
 private:
+  static const size_t CHUNK_SIZE = 8192;
+
   bit_sequence buf;
   std::istream& input;
-  size_t chunk_size;
   size_t pos;
-  uint8_t nlb;
-  void fetch(size_t n);
+  uint8_t unused;
+  void fetch();
   bool eof();
+  size_t buf_size() const;
 };
 
 #endif // HUFFMAN_INPUT_WRAPPER_H
